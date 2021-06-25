bb653be90a16413550e156412abb6f33bc39306e
acer <acer@DESKTOP-LOUKCQ3>
2021-06-25 14:15:01 +0530

    first_commit

---
+++ ToDoApp/src/main/java/com/ctrl/Homectrl.java
@@ -0,0 +1,17 @@
+package com.ctrl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Homectrl {
+	
+	@RequestMapping("/home")
+	public String Home() {
+		
+		return "home"; 
+				
+	}
+
+}
